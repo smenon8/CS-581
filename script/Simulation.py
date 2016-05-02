b0075eb3bd09602761d875003d2de2787b1d9821
Sreejith Menon <sreejithmenon@Sreejiths-MacBook-Pro.local>
2016-05-01 23:12:36 -0500

    Adding real time algorithm and more comments

--- script/Simulation.py
+++ script/Simulation.py
@@ -3,17 +3,15 @@ import importlib
 import csv
 importlib.reload(UDB)
 
+# Expected parameters: Algorithm or method name, congestion level and name of the output csv file expeceted
 
 def gatherData(methName,conjestion,outFile):
    
     cnx = UDB.connectToMySQL()
     cursor = cnx.cursor()
 
-    #Create a list of date timestamps [Should be 4, verify with rajan and sreejith]
-    datetimestampList = ['2012-04-11 17:30:00','2012-04-06 12:40:00'] # ,'2012-04-06 21:45:00','2012-04-06 09:45:00']
-
-    # datetimestampList = ['2012-04-06 00:06:32', '2012-05-02 18:56:18','2012-04-07 13:21:35','2012-04-08 16:39:34',
-                        #'2012-04-18 16:49:42','2012-04-09 9:28:31','2012-04-10 9:28:31','2012-04-13 22:40:53']
+    datetimestampList = ['2012-04-06 00:06:32', '2012-05-02 18:56:18','2012-04-07 13:21:35','2012-04-08 16:39:34',
+                        '2012-04-18 16:49:42','2012-04-09 9:28:31','2012-04-10 9:28:31','2012-04-13 22:40:53']
 
     wrtFLcontents = []
     for datetimestamp in datetimestampList:
